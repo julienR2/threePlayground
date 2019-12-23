c49e9676a6b23e51d2e178a2321e79596d1b3aab
Alex <sibony.alexandre@gmail.com>
2019-12-23 19:00:01 +0100

    added planet information

--- src/threejs/Worlds/SpaceXWorld/planets.js
+++ src/threejs/Worlds/SpaceXWorld/planets.js
@@ -5,6 +5,7 @@
 export default {
   mercury: {
     planet_code: 'mercury',
+    ref: 'star_sun',
     distance_to_ref: 57,
     diameter: 4878,
     orbit_revolution: 364.25,
@@ -12,6 +13,7 @@ export default {
   },
   venus: {
     planet_code: 'venus',
+    ref: 'star_sun',
     distance_to_ref: 108.208,
     diameter: 12104,
     orbit_revolution: 225,
@@ -19,6 +21,7 @@ export default {
   },
   earth: {
     planet_code: 'earth',
+    ref: 'star_sun',
     distance_to_ref: 150,
     diameter: 12742,
     orbit_revolution: 365.24,
@@ -26,6 +29,7 @@ export default {
   },
   mars: {
     planet_code: 'mars',
+    ref: 'star_sun',
     distance_to_ref: 228,
     diameter: 6787,
     orbit_revolution: 687,
@@ -33,21 +37,46 @@ export default {
   },
   jupiter: {
     planet_code: 'jupiter',
+    ref: 'star_sun',
     distance_to_ref: 778.4,
     diameter: 139822,
-    orbit_revolution: 52122,
     rotation_period: 0.4,
+    orbit_revolution: 4380,
   },
-  // saturn: {
-  //   planet_code: 'saturn',
-  //   distance_to_ref: 1433.4,
-  // },
-  // uranus: {
-  //   planet_code: 'uranus',
-  //   distance_to_ref: 3000,
-  // },
-  // neptune: {
-  //   planet_code: 'neptune',
+  saturn: {
+    planet_code: 'saturn',
+    ref: 'star_sun',
+    distance_to_ref: 1433.4,
+    diameter: 116460,
+    orbit_revolution: 10585,
+  },
+  uranus: {
+    planet_code: 'uranus',
+    ref: 'star_sun',
+    distance_to_ref: 3000,
+    diameter: 50724,
+    orbit_revolution: 30660,
+  },
+  neptune: {
+    planet_code: 'neptune',
+    ref: 'star_sun',
+    distance_to_ref: 4500.4,
+    diameter: 49244,
+    orbit_revolution: 60225,
+  },
+  pluto: {
+    planet_code: 'pluto',
+    ref: 'star_sun',
+    planet_type: 'dwarf',
+    distance_to_ref: 5900.898440,
+    diameter: 2376.6,
+    orbit_revolution: 90520,
+  },
+  // planet_nine: {
+  //   planet_code: 'planet_nine',
+  //   ref: 'star_sun',
   //   distance_to_ref: 4500.4,
+  //   orbit_revolution: 60225,
+  //   diameter: 139822,
   // },
 }
