6ae73e61d80fbe12ae2db4b01fb28c8efa4b6c0b
Alex <sibony.alexandre@gmail.com>
2019-12-23 19:00:23 +0100

    added orbits object to spaceX

---
+++ src/threejs/Components/SpaceX/Orbit.js
@@ -0,0 +1,27 @@
+import Ellipse from '../Ellipse'
+
+const Orbit = ({
+  geometry,
+  material,
+  radius,
+  position = [0, 0, 0],
+  orbit = true,
+  data,
+} = {}) => {
+  // const mesh = Ellipsis()
+  const mesh = Ellipse(
+    {
+
+    }
+  )
+
+
+
+
+
+
+
+  return { mesh }
+}
+
+export default Orbit
